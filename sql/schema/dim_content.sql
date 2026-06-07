6d2ad8b20db77fc13cab1a0af6ae32cdc4016a67
TeodoraDima11001 <8m6smjf28y@privaterelay.appleid.com>
2026-06-08 00:54:10 +0100

    Update dim_content.sql

--- sql/schema/dim_content.sql
+++ sql/schema/dim_content.sql
@@ -1,11 +1,5 @@
-CREATE TABLE dim_content (
-    content_id VARCHAR(50) PRIMARY KEY,
-    title VARCHAR(255),
-    creator_id VARCHAR(50),
-    language VARCHAR(50),
-    content_format VARCHAR(50),
-    genre VARCHAR(100),
-    subgenre VARCHAR(100),
-    mood VARCHAR(100),
-    release_date DATE
-);
+SELECT
+    content_id,
+    'unknown' AS title,
+    'unknown' AS category
+FROM read_csv_auto('data/raw/content_events.csv');
