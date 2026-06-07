e4d3d49c61fbd17ac9a4be18445740492138d20d
TeodoraDima11001 <8m6smjf28y@privaterelay.appleid.com>
2026-06-07 23:34:54 +0100

    Update stg_events.sql

--- dbt/models/staging/stg_events.sql
+++ dbt/models/staging/stg_events.sql
@@ -1,9 +1,7 @@
 SELECT
-    event_id,
-    content_id,
     user_id,
-    event_timestamp,
-    event_type,
-    watch_time_seconds,
-    completion_rate
-FROM {{ source('raw', 'fact_content_events') }}
+    content_id,
+    watch_time,
+    click,
+    content_type
+FROM {{ source('raw', 'content_events') }}
