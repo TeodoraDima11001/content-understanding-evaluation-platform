972a4ef26af656e5a1f1f196bad395bf4eee5971
TeodoraDima11001 <8m6smjf28y@privaterelay.appleid.com>
2026-06-08 00:57:06 +0100

    Update fact_content_events.sql

--- sql/schema/fact_content_events.sql
+++ sql/schema/fact_content_events.sql
@@ -1,7 +1,17 @@
 SELECT
-    user_id,
-    content_id,
-    watch_time,
-    click,
-    content_type
-FROM read_csv_auto('data/raw/content_events.csv');
+    e.user_id,
+    e.content_id,
+    u.age,
+    u.country,
+    u.subscription,
+    e.watch_time,
+    e.click,
+
+    CASE 
+        WHEN e.watch_time > 20 THEN 1 
+        ELSE 0 
+    END AS high_engagement
+
+FROM read_csv_auto('data/raw/content_events.csv') e
+LEFT JOIN read_csv_auto('data/raw/users.csv') u
+ON e.user_id = u.user_id;
