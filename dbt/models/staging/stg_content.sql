64d42b3e3c705ea0196a00afd1035689170c24b5
TeodoraDima11001 <8m6smjf28y@privaterelay.appleid.com>
2026-06-07 23:35:48 +0100

    Update stg_content.sql

--- dbt/models/staging/stg_content.sql
+++ dbt/models/staging/stg_content.sql
@@ -1,7 +1,14 @@
 SELECT
-    content_id,
-    title,
-    genre,
-    subgenre,
-    language
-FROM {{ source('raw', 'dim_content') }}
+    e.user_id,
+    e.content_id,
+    u.country,
+    u.subscription,
+    e.watch_time,
+    e.click,
+    CASE 
+        WHEN e.watch_time > 20 THEN 1 
+        ELSE 0 
+    END AS high_engagement
+FROM {{ ref('stg_events') }} e
+LEFT JOIN {{ ref('stg_user') }} u
+    ON e.user_id = u.user_id
