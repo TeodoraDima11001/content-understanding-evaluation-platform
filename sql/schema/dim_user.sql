b7c9e7e2e3f992e8d57ed1c455bd252eb74d3b74
TeodoraDima11001 <8m6smjf28y@privaterelay.appleid.com>
2026-06-08 00:55:38 +0100

    Update dim_user.sql

--- sql/schema/dim_user.sql
+++ sql/schema/dim_user.sql
@@ -1,6 +1,6 @@
-CREATE TABLE dim_user (
-    user_id VARCHAR(50) PRIMARY KEY,
-    country VARCHAR(50),
-    subscription_type VARCHAR(50),
-    signup_date DATE
-);
+SELECT
+    user_id,
+    age,
+    country,
+    subscription
+FROM read_csv_auto('data/raw/users.csv');
