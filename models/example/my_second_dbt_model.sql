88ec8bda358eaed506ec10cacbe0082eab8f0de4
TeodoraDima11001 <8m6smjf28y@privaterelay.appleid.com>
2026-06-09 23:15:10 +0100

    Update my_second_dbt_model.sql

--- models/example/my_second_dbt_model.sql
+++ models/example/my_second_dbt_model.sql
@@ -1,12 +1,2 @@
-{{ config(materialized='table') }}
-
-with source_data as (
-
-    select 10 as id
-    union all
-    select 20 as id
-
-)
-
-select *
-from source_data
+select id
+from {{ ref('my_first_dbt_model') }}
